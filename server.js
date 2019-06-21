e5cd84504f3bb05d0f7e23e45aba86f92e1354d6
Jake Waltrip <jake.waltrip@gmail.com>
2019-06-20 20:42:21 -0500

    added initial server

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config()
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
+
+const app = express()
+
+// import routes
+// const userRoutes = require("./routes/UserRoute");
+
+// setup middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// setup routes
+// app.use('/api/users', userRoutes);
+
+// set the backend server port
+const port = process.env.PORT || 5000
+
+// a catchall route if any API calls aren't used, then serve the index.html built by react
+// this needs to be after all other routes
+// used for when deoployed to Heroku
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+app.listen(port, () => {
+  console.log(`Backend server running and listening on port ${port}`)
+})
+
+module.exports = app
